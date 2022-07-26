ed22d8edc3d71c5df0a6dc3e6ec53cbebeef42bb
CSE-AMITKABI <kabiamit12@gmail.com>
2022-07-26 08:42:08 +0530

    adding dynamic content in webpage

--- todolists/models.py
+++ todolists/models.py
@@ -1,9 +1,9 @@
+from turtle import heading
 from django.db import models
 
 # Create your models here.
 class TodoList(models.Model):
     text = models.CharField(max_length=50)
     completed = models.BooleanField(default=False)
-
     def __str__(self):
         return self.text
--- todolists/views.py
+++ todolists/views.py
@@ -1,5 +1,10 @@
+from multiprocessing import context
 from django.shortcuts import render
+
+from .models import TodoList
 
 # Create your views here.
 def index(request):
-    return render(request,'todolists/index.html')
+    todo_items = TodoList.objects.order_by('id')
+    context = {'todo_items':todo_items} 
+    return render(request,'todolists/index.html',context)
